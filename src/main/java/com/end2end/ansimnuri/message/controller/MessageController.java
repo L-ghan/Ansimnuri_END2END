aba54fb9816367378946ca21b0ee08ba9ebe2aad
MoondaeGI <ansgur119977@gmail.com>
2025-05-06 01:00:11 +0900

    add @Tag annotation to controllers

--- src/main/java/com/end2end/ansimnuri/message/controller/MessageController.java
+++ src/main/java/com/end2end/ansimnuri/message/controller/MessageController.java
@@ -1,10 +1,12 @@
 package com.end2end.ansimnuri.message.controller;
 
 import com.end2end.ansimnuri.message.service.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "메세지 API", description = "메세지 CURD, 메세지 차단 기능을 가진 API")
 @RequiredArgsConstructor
 @RequestMapping("/message")
 @RestController
--- src/main/java/com/end2end/ansimnuri/note/controller/NoteController.java
+++ src/main/java/com/end2end/ansimnuri/note/controller/NoteController.java
@@ -1,10 +1,12 @@
 package com.end2end.ansimnuri.note.controller;
 
 import com.end2end.ansimnuri.note.service.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="쪽지 API", description = "쪽지 CRUD, 쪽지 추천, 쪽지 답글 CRUD 기능을 가진 API")
 @RequiredArgsConstructor
 @RequestMapping("/note")
 @RestController
