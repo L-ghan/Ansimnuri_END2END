8388fbf78ea5679157ec06a72a6e8ea31e6595ec
MoondaeGI <ansgur119977@gmail.com>
2025-05-06 00:39:34 +0900

    add openapi annotation to NoticeController

--- src/main/java/com/end2end/ansimnuri/board/controller/NoticeController.java
+++ src/main/java/com/end2end/ansimnuri/board/controller/NoticeController.java
@@ -21,6 +21,7 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @Operation(summary = "전체 공지사항 조회 api", description = "모든 공지사항 내용을 가져온다.")
+    @ApiResponse(responseCode = "200", description = "정상 작동입니다.")
     @GetMapping()
     public ResponseEntity<List<NoticeDTO>> selectAll() {
         return ResponseEntity.ok(noticeService.selectAll());
@@ -39,18 +40,33 @@ public class NoticeController {
     }
 
     @Operation(summary = "공지사항 등록 api", description = "공지사항을 등록한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 작동입니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력값을 넣었습니다."),
+            @ApiResponse(responseCode = "403", description = "해당 요청을 사용할 권한이 없습니다.")
+    })
     @PostMapping()
     public ResponseEntity<Void> insert(@RequestBody NoticeDTO noticeDTO) {
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "공지사항 수정 api", description = "공지사항을 수정한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 작동입니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력값을 넣었습니다."),
+            @ApiResponse(responseCode = "403", description = "해당 요청을 사용할 권한이 없습니다.")
+    })
     @PutMapping()
     public ResponseEntity<Void> update(@RequestBody NoticeDTO noticeDTO) {
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "공지사항 삭제 api", description = "해당 ID에 맞는 공지사항을 삭제한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 작동입니다."),
+            @ApiResponse(responseCode = "400", description = "해당 id의 공지사항이 존재하지 않습니다."),
+            @ApiResponse(responseCode = "403", description = "해당 요청을 사용할 권한이 없습니다.")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(
             @Parameter(description = "공지사항 id")
