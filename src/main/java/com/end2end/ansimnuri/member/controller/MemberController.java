74715d838cc65a587fa3d9c4836d0b11d51d75e8
yoonah <rkddbsdk3@naver.com>
2025-05-08 17:23:07 +0900

    에러진정

--- src/main/java/com/end2end/ansimnuri/member/controller/MemberController.java
+++ src/main/java/com/end2end/ansimnuri/member/controller/MemberController.java
@@ -2,15 +2,12 @@ package com.end2end.ansimnuri.member.controller;
 
 import com.end2end.ansimnuri.member.dto.MemberDTO;
 import com.end2end.ansimnuri.member.service.MemberService;
-import com.end2end.ansimnuri.member.service.UserService;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,13 +18,14 @@ import java.util.Map;
 @RestController
 public class MemberController {
     private final MemberService memberService;
-
-    public String login(MemberDTO dto, HttpServletRequest request) {
-        MemberDTO user = memberService.login(dto);
-        if (user == null) {
-            return "redirect:/";
-        }
-        return "redirect:/";
+@GetMapping("/login")
+    public String login(@RequestBody MemberDTO dto, HttpServletRequest request) {
+//        MemberDTO user = memberService.login(dto);
+//        if (user == null) {
+//            return "redirect:/";
+//        }
+//        return "redirect:/";
+    return null;
 
     }
     @GetMapping("/checkId/{id}")
