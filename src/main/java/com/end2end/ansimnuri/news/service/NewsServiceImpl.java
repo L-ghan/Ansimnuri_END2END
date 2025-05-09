42619205eb07da6f5340c871e7e0d94044777dd9
inho <yuho93@naver.com>
2025-05-09 10:59:53 +0900

    str

--- src/main/java/com/end2end/ansimnuri/news/service/NewsServiceImpl.java
+++ src/main/java/com/end2end/ansimnuri/news/service/NewsServiceImpl.java
@@ -1,9 +1,23 @@
 package com.end2end.ansimnuri.news.service;
 
+import com.end2end.ansimnuri.news.dto.NewsDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class NewsServiceImpl implements NewsService {
+    @Value("${news.api.key}")
+    private String apiKey;
+
+    @Override
+    public List<NewsDTO> fetchNews() {
+        String url = "https://newsapi.org/v2/everything?"
+                + "q=범죄&살인&상해&폭행&묻지마&강도&절도"
+                + "&apiKey=" + apiKey;
+
+    }
 }
