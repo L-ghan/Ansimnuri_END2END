ea817392b5987d4378d4bf75773f37c9269cf33a
lgh <dlarmsgks@naver.com>
2025-05-19 19:36:55 +0900

    get pull request

---
+++ src/main/java/com/end2end/ansimnuri/member/controller/DashboardController.java
@@ -0,0 +1,23 @@
+package com.end2end.ansimnuri.member.controller;
+
+import com.end2end.ansimnuri.member.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/dashboard")
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping("/top-addresses")
+    public ResponseEntity<List<Map<String, Object>>> getTopAddresses() {
+        List<Map<String, Object>> topAddresses = dashboardService.getTop5Addresses();
+        return ResponseEntity.ok(topAddresses);
+    }
+}
