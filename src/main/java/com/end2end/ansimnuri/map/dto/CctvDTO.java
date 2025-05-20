05a77e8741f6a33536f90c7b2b19b90f60235a27
MoonDeaGI <ansgur119977@gmail.com>
2025-05-20 15:56:49 +0900

    update schema annotation in dtos

--- src/main/java/com/end2end/ansimnuri/map/dto/CctvDTO.java
+++ src/main/java/com/end2end/ansimnuri/map/dto/CctvDTO.java
@@ -1,6 +1,7 @@
 package com.end2end.ansimnuri.map.dto;
 
 import com.end2end.ansimnuri.map.domain.entity.Cctv;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,16 +9,23 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Schema(description = "CCTV DTO")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CctvDTO {
+    @Schema(description = "id", example = "1", minimum = "1")
     private long id;
+    @Schema(description = "위도", example = "126.927112885")
     private double latitude;
+    @Schema(description = "경도", example = "37.490042567")
     private double longitude;
+    @Schema(description = "주소", example = "서울특별시 관악구 봉천로 227 (봉천동)")
     private String address;
+    @Schema(description = "카메라 대수", example =  "3", minimum = "1")
     private int cameraCount;
+    @Schema(description = "설치 일자", example = "2024-10-01")
     private LocalDate installDate;
 
     public static CctvDTO of(Cctv cctv) {
