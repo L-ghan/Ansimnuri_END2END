faf72c069efe6756543ab8b0c26d687ec3e30555
MoonDeaGI <ansgur119977@gmail.com>
2025-05-20 15:40:19 +0900

    remove unnecessary package

--- src/main/java/com/end2end/ansimnuri/message/domain/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.end2end.ansimnuri.message.domain.repository;
-
-import com.end2end.ansimnuri.message.domain.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
